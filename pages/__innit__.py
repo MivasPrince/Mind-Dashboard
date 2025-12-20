7b7f424748008a738a23078dc935defa4c7bb192
MivasPrince <prince@miva.university>
2025-12-20 03:54:36 +0100

    Create __innit__.py

---
+++ pages/__innit__.py
@@ -0,0 +1,8 @@
+"""
+Dashboard pages package
+Contains all dashboard page modules (home, student, faculty, developer, admin)
+"""
+
+from pages import home, student, faculty, developer, admin
+
+__all__ = ['home', 'student', 'faculty', 'developer', 'admin']
